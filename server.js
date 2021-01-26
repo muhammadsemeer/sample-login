7968c15f046eb005aceb7926da382d4e8f14af9e
muhammadsemeer <muhammadsemeerali@gmail.com>
2021-01-26 17:29:04 +0530

    cors updated

--- server.js
+++ server.js
@@ -16,7 +16,12 @@ db.connect((err) => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://pkmbilal.github.io/",
+    credentials: true,
+  })
+);
 
 //Routes
 app.post("/signup", async (req, res) => {
